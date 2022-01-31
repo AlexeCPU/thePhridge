95277499594eb2d5bdcc5e39b18599dcda25171b
AlexeCPU <23burkschase@mcnairy.org>
2022-01-30 21:16:26 -0600

    Finished UI windows changing.
    Signed-off-by: AlexeCPU <23burkschase@mcnairy.org>

--- main/root.py
+++ main/root.py
@@ -1,7 +1,7 @@
 ﻿# This Python file uses the following encoding: utf-8
 import sys
 from PySide6.QtWidgets import QMainWindow
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 from mainwin import Ui_MainWindow
 
 
@@ -12,10 +12,27 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
 
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+
+    #functions
+
+
+
+    #Buttons and crap goes here
+
+    ui.Home.clicked.connect(lambda : ui.stackedWidget.setCurrentIndex(1))
+    ui.Music.clicked.connect(lambda : ui.stackedWidget.setCurrentIndex(0))
+    ui.CalcButt.clicked.connect(lambda : ui.stackedWidget.setCurrentIndex(2))
+
     MainWindow.show()
     sys.exit(app.exec())
+
+
+
+
