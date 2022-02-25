ade7b121250e9ef8dc81a1075331af3e1b9e8b38
Phridge-Local <rupprechtzach@gmail.com>
2022-02-25 13:16:04 -0600

    A whole ton of changes that were made without intermiddent commite

--- main/calc.py
+++ main/calc.py
@@ -1,23 +1,58 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide2.QtWidgets import QMainWindow
-from PySide2 import QtWidgets, QtCore
+from PySide2.QtWidgets import QMainWindow, QLabel, QListWidgetItem, QMessageBox
+from PySide2.QtCore import QRect
 from mainwin import Ui_MainWindow
+from popup_window import PopupWindow
 
 
 
 class Calc(QMainWindow):
 	def init(self, ui):    
-		ui.calcText.setText("")
-		ui.calcBackButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text()[0 : -1]))
-		ui.calcAddButton.clicked.connect(lambda : ui.calcText.setText(""))
-		ui.calcOneButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "1"))
-		ui.calcTwoButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "2"))
-		ui.calcThreeButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "3"))
-		ui.calcFourButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "4"))
-		ui.calcFiveButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "5"))
-		ui.calcSixButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "6"))
-		ui.calcSevenButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "7"))
-		ui.calcEightButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "8"))
-		ui.calcNineButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "9"))
-		ui.calcZeroButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "0"))
+		self.add_item_box = QMessageBox()
+		self.add_item_box.setWindowTitle("Add New Item")
+		self.add_item_box.setText("test text!")
+		# ui.calcText.setText("")
+		# ui.calcBackButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text()[0 : -1]))
+		ui.addItemToListButton.clicked.connect(lambda : self.handle_add_to_list(ui))
+		# ui.calcOneButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "1"))
+		# ui.calcTwoButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "2"))
+		# ui.calcThreeButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "3"))
+		# ui.calcFourButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "4"))
+		# ui.calcFiveButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "5"))
+		# ui.calcSixButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "6"))
+		# ui.calcSevenButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "7"))
+		# ui.calcEightButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "8"))
+		# ui.calcNineButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "9"))
+		# ui.calcZeroButton.clicked.connect(lambda : ui.calcText.setText(ui.calcText.text() + "0"))
+		ui.addHundCalsServ.clicked.connect(lambda: ui.calServ.setText(str(int(ui.calServ.text()) + 100)))
+		ui.addTenCalsServ.clicked.connect(lambda: ui.calServ.setText(str(int(ui.calServ.text()) + 10)))
+		ui.minusTenCalsServ.clicked.connect(lambda: ui.calServ.setText(str(int(ui.calServ.text()) - 10)))
+		ui.minusHundCalsServ.clicked.connect(lambda: ui.calServ.setText(str(int(ui.calServ.text()) - 100)))
+		ui.addFiveServ.clicked.connect(lambda: ui.serv.setText(str(int(ui.serv.text()) + 5)))
+		ui.addOneServ.clicked.connect(lambda: ui.serv.setText(str(int(ui.serv.text()) + 1)))
+		ui.minusOneServ.clicked.connect(lambda: ui.serv.setText(str(int(ui.serv.text()) - 1)))
+		ui.minusFiveServ.clicked.connect(lambda: ui.serv.setText(str(int(ui.serv.text()) - 5)))
+		ui.addItemButton.clicked.connect(lambda: self.add_item(ui))
+		ui.servings.verticalScrollBar().valueChanged.connect(
+            ui.foodName.verticalScrollBar().setValue)
+		ui.servings.verticalScrollBar().valueChanged.connect(
+            ui.calPerServing.verticalScrollBar().setValue)
+
+	def handle_add_to_list(self, ui):
+		ui.stackedWidget.setCurrentIndex(3)
+		ui.calServ.setText("0")
+		ui.serv.setText("0")
+		ui.lineEdit.setText("")
+
+	def add_item(self, ui):
+		ui.stackedWidget.setCurrentIndex(2)
+		foodName = QListWidgetItem()
+		foodName.setText(ui.lineEdit.text())
+		ui.foodName.addItem(foodName)
+		calPerServing = QListWidgetItem()
+		calPerServing.setText(ui.calServ.text())
+		ui.calPerServing.addItem(calPerServing)
+		servings = QListWidgetItem()
+		servings.setText(ui.serv.text())
+		ui.servings.addItem(servings)
--- main/root.py
+++ main/root.py
@@ -1,18 +1,77 @@
 ﻿# This Python file uses the following encoding: utf-8
-import sys
-from PySide2.QtWidgets import QMainWindow
+import time, sys
+from PySide2.QtWidgets import QMainWindow, QSplashScreen
+from PySide2.QtGui import QPixmap
+from PySide2.QtCore import Qt
 from PySide2 import QtWidgets, QtCore
 from mainwin import Ui_MainWindow
-from calc import Calc
 
+from calc import Calc
+from settings import Settings
+from visualizer import GLWidget
+
+frame = 0
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
+
+        self.glWidget = GLWidget()
+
+        self.glWidgetArea = QtWidgets.QScrollArea()
+        self.glWidgetArea.setWidget(self.glWidget)
+        self.glWidgetArea.setWidgetResizable(True)
+        self.glWidgetArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.glWidgetArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.glWidgetArea.setSizePolicy(QtWidgets.QSizePolicy.Ignored,
+                QtWidgets.QSizePolicy.Ignored)
+        self.glWidgetArea.setMinimumSize(50, 50)
+
+        # sliders
+        x_slider = self.create_slider(self.glWidget.x_rotation_changed,
+                                     self.glWidget.set_x_rot_speed)
+        y_slider = self.create_slider(self.glWidget.y_rotation_changed,
+                                     self.glWidget.set_y_rot_speed)
+        z_slider = self.create_slider(self.glWidget.z_rotation_changed,
+                                     self.glWidget.set_z_rot_speed)
+
+        # set the layout
+        central_layout = QtWidgets.QVBoxLayout()
+        central_layout.addWidget(self.glWidgetArea)
+        central_layout.addWidget(x_slider)
+        central_layout.addWidget(y_slider)
+        central_layout.addWidget(z_slider)
+        central_widget.setLayout(central_layout)
+
+        x_slider.setValue(1)
+        y_slider.setValue(1)
+        z_slider.setValue(0)
+
+        self.setWindowTitle("Hello OpenGL")
+        self.resize(400, 300)
+
+    def create_slider(self, changedSignal, setterSlot):
+        """Helper to create a slider."""
+        slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        slider.setRange(0, 10)
+        slider.setSingleStep(1)
+        slider.setPageStep(5)
+        slider.setTickInterval(2)
+        slider.setTickPosition(QtWidgets.QSlider.TicksRight)
+
+        slider.valueChanged.connect(setterSlot)
+        changedSignal.connect(slider.setValue)
+
+        return slider
 
 
+def updateVoicePack(ui):
+    settings.setValue("voice_pack",ui.voiceChoiceBox.currentText())
+    settings.setValue("voice_pack_index",ui.voiceChoiceBox.currentIndex())
 
 
 if __name__ == "__main__":
@@ -20,12 +79,38 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    geometry = app.desktop().availableGeometry()
+
+    settings = Settings()
+    settings.init("settings.json")
+
+    MainWindow.setGeometry(geometry)
+    MainWindow.setWindowFlag(Qt.FramelessWindowHint)
 
     #functions
 
+    if(len(sys.argv) < 2 or sys.argv[1] != "--no-splash"):
+        frame = 1
+        pixmap = QPixmap((f"/home/pi/the splash/splash000.jpg")).scaled(app.desktop().screenGeometry().width(), app.desktop().screenGeometry().height())
+        splash=QSplashScreen(pixmap)
+        splash.show()
 
+        while frame != 420:
+            milliseconds = int(round(time.time() * 1000))
+            pixmap = QPixmap((f"/home/pi/the splash/splash{frame:03}.jpg")).scaled(app.desktop().screenGeometry().width(), app.desktop().screenGeometry().height())
+            splash.setPixmap(pixmap)
+            frame+=1
+            time.sleep(max((1/60) - ((int(round(time.time() * 1000)) - milliseconds)/1000),0))
+            app.processEvents()
+        splash.hide()
 
     #Buttons and crap goes here
+
+    ui.autoCloseSpinBox.setValue(int(settings.getValue("auto_off"))//60)
+    ui.autoCloseSpinBox.valueChanged.connect(lambda: settings.setValue("auto_off",ui.autoCloseSpinBox.value()*60))
+
+    ui.voiceChoiceBox.setCurrentIndex(settings.getValue("voice_pack_index"))
+    ui.voiceSetButton.clicked.connect(lambda: updateVoicePack(ui))
 
     calc = Calc()
     calc.init(ui)
@@ -33,9 +118,12 @@ if __name__ == "__main__":
     ui.Home.clicked.connect(lambda : ui.stackedWidget.setCurrentIndex(1))
     ui.Music.clicked.connect(lambda : ui.stackedWidget.setCurrentIndex(0))
     ui.CalcButt.clicked.connect(lambda : ui.stackedWidget.setCurrentIndex(2))
+    ui.Settings.clicked.connect(lambda : ui.stackedWidget.setCurrentIndex(4))
 
     MainWindow.show()
-    sys.exit(app.exec_())
+    res = app.exec_()
+    MainWindow.glWidget.free_resources()
+    sys.exit(res)
 
 
 
